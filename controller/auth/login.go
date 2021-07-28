c1f6a2f0a7d2a5236140d62f3ca7b2a163745689
Lenar Batdalov <developer.lenar@gmail.com>
2021-07-28 08:48:12 +0300

    первый layout, работающий корректно

--- controller/auth/login.go
+++ controller/auth/login.go
@@ -1,1 +1,11 @@
 package auth
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func LoginController(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "", nil)
+}
--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	"github.com/lenarbatdalov/go-application/service"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
 )
 
 var (
+	sessionSecretKey = "secret"
+	store            = cookie.NewStore([]byte(sessionSecretKey))
 	// db = database.InitConnect()
-	s = service.Server()
 )
 
 func main() {
-	err := s.Run(":8080")
+	server := gin.New()
+
+	server.Use(sessions.Sessions("mysession", store))
+	server.LoadHTMLGlob("templates/**/*.html")
+	server.Static("/assets", "./assets")
+
+	server.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index", gin.H{
+			"title": "Home Page",
+			"name":  "Lenar",
+		})
+	})
+
+	err := server.Run(":8080")
 	if err != nil {
 		panic("server not started")
 	}
